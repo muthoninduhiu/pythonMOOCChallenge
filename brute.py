4d5781a18555525e2de57982e579cd488df3d4a7
muthoninduhiu <muthoninduhiu.mn@gmail.com>
2023-02-19 13:40:11 +0300

    final word list brute force file

--- brute.py
+++ brute.py
@@ -1,29 +1,17 @@
 import tkinter as tk
 import hashlib
-from urllib.request import urlopen
+import time
 
 # create the main window of the app
 root = tk.Tk()
 # set the title of the main window as 'Brute Force'
 root.title("Brute Force")
-root.geometry("600x400")
+root.geometry("1000x500")
 root.config(background="#000000")
 
 
-# functions
-def read_word_list(url):
-    """
-    A function used to read the list of guess password
-    """
-    try:
-        word_list_file = urlopen(url).read()
-    except Exception as e:
-        print("Hey there was some error while reading the wordlist, error:", e)
-        exit()
-    return word_list_file
-
-
-def get_hash(password):
+# functions #
+def get_hash_password(password):
     """
     A function used return the password as a string of double length
     """
@@ -31,18 +19,19 @@ def get_hash(password):
     return result.hexdigest()
 
 
-def bruteforce(guess_password_list, password_hash):
-    for guess_password in guess_password_list:
-        if hash(guess_password) == password_hash:
-            correct_password = guess_password
+def bruteforce(password_list, hashed_password):
+    for guessed_password in password_list:
+        if get_hash_password(guessed_password) == hashed_password:
+            correct_password = guessed_password
             answer = tk.Label(root, text="Your password is", font=("Verdana", 20), bg="#000000", fg="#1e00ff")
             answer.place(x=350, y=310)
-            answer.config(text="Your password is \n" + correct_password)
+            answer.config(text="Your password is \n" + correct_password
+                               + "\n please change this, it was really easy to guess it (:")
             print('your password is:', correct_password)
-        # else:
-        #    no_answer = tk.Label(root, text="no list", font=("Verdana", 20), bg="#000000", fg="#1e00ff")
-        #    no_answer.place(x=350, y=310)
-        #    no_answer.config(text="Your password is not in the word list")
+        else:
+            no_answer = tk.Label(root, text="no list", font=("Verdana", 20), bg="#000000", fg="#1e00ff")
+            no_answer.place(x=350, y=310)
+            no_answer.config(text="Your password is not in the word list")
 
 
 def check_password():
@@ -54,21 +43,31 @@ def check_password():
     password_check.place(x=120, y=310)
     password_check.config(text="Hacking... \n" + password)
 
-    url = 'https://raw.githubusercontent.com/berzerk0/Probable-Wordlists/master/Real-Passwords/Top12Thousand-probable' \
-          '-v2.txt'
-    password_hash = get_hash(password)
-    wordlist = read_word_list(url).decode('UTF-8')
-    guess_password_list = wordlist.split('\n')
+    with open('password.txt', 'r', encoding='utf-8') as text_file:
+        contents = text_file.read()
+
+    st = time.time()
+    hashed_password = hash(password)
+    password_list = contents.split('\n')
     # Running the Brute Force attack
-    bruteforce(guess_password_list, password_hash)
+    bruteforce(password_list, hashed_password)
+
+    # get the end time
+    et = time.time()
+
+    # get the execution time
+    res = et - st
+    final_res = res * 1000
+    print('Execution time:', final_res, 'milliseconds')
+    time_used = tk.Label(root, text="Execution time", font=("Verdana", 20), bg="#000000", fg="#1e00ff")
+    time_used.place(x=350, y=400)
+    time_used.config(text="Execution time: " + str(final_res) + 'milliseconds')
 
 
-# interface
+# interface #
 
 heading = tk.Label(root, text="Password Hacker", font=("Verdana", 23, "bold"), bg="#000000", fg="#1e00ff")
 heading.pack(pady=(90, 0))
-# subheading = Label(root, text="Check how long it takes to brute force your password.", font=("Courier New", 20,
-# "bold"), bg="#000000", fg="#16FF00") subheading.pack(pady=(10, 0))
 
 enter_text = tk.Entry(root, justify="center", width=18, font=("Verdana", 20, "bold"), bg="white", border=2)
 enter_text.pack(pady=10)
