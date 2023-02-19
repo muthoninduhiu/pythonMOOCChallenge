b1426f4f5f088927b396992c051ecd15f9c7ab68
muthoninduhiu <muthoninduhiu.mn@gmail.com>
2023-02-19 13:39:00 +0300

    add original brute force code

--- bruteForce.py
+++ bruteForce.py
@@ -12,18 +12,43 @@ frame.pack()
 # password = ""
 
 
+
+with open('password.txt', 'r', encoding='utf-8') as text_file:
+    contents = text_file.read()
+
+actual_password = input("enter password to test: ")
+
+guess_password_list = contents.split('\n')
+
+
+def hash_password(word_list_password):
+    result = hashlib.sha1(word_list_password.encode())
+    return result.hexdigest()
+
+
+def bruteforce(guess_password_list, actual_password_hash):
+    for guess_password in guess_password_list:
+        if hash(guess_password) == actual_password_hash:
+            print("Hey! your password is:", guess_password,
+                  "\n please change this, it was really easy to guess it (:")
+            # If the password is found then it will terminate the script here
+            exit()
+
+
 def check_password():
     password = enter_text.get()
     password_check = tk.Label(root, text="Hacking...", font=("Verdana", 20), bg="#000000", fg="#1e00ff")
     password_check.place(x=120, y=310)
     password_check.config(text="Hacking... \n" + password)
+    # hash password with hash library
+    actual_password_hash = hash_password(actual_password)
+
+    # Running the Brute Force attack
+    bruteforce(guess_password_list, actual_password_hash)
 
 
-heading = tk.Label(root, text="Password Hacker", font=("Verdana", 23, "bold"), bg="#000000", fg="#1e00ff")
+heading = tk.Label(root, text="Password Checker", font=("Verdana", 23, "bold"), bg="#000000", fg="#1e00ff")
 heading.pack(pady=(90, 0))
-# subheading = Label(root, text="Check how long it takes to brute force your password.", font=("Courier New", 20,
-# "bold"), bg="#000000", fg="#16FF00") subheading.pack(pady=(10, 0))
-
 enter_text = tk.Entry(root, justify="center", width=18, font=("Verdana", 20, "bold"), bg="white", border=2)
 enter_text.pack(pady=10)
 enter_text.focus()
