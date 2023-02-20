00d0d0bf47d95e5e500eab69833aa99eb671dcbd
muthoninduhiu <muthoninduhiu.mn@gmail.com>
2023-02-20 11:36:48 +0300

    add comments and consistency to the code

--- brute_force_crack.py
+++ brute_force_crack.py
@@ -13,6 +13,8 @@ root.config(background="#000000")
 
 
 # functions
+
+# function to add text to entry text in tkinter
 def temp_text(e):
     enter_text.delete(0, "end")
 
@@ -20,28 +22,41 @@ def temp_text(e):
 def get_hash(password):
     """
     A function used return the password as a string of double length
+    param: password
+    return: password that has been hashed
     """
     result = hashlib.sha1(password.encode())
     return result.hexdigest()
 
 
-def bruteforce(guess_password_list, password_hash, password, try_random):
-    for guess_password in guess_password_list:
-        if get_hash(guess_password) == password_hash:
+def brute_force(password_list, hashed_password, password, try_random_password):
+    """
+        A function used return the password as a string of double length
+        param: password: user input,
+               password_hash: hashed password,
+               guess_password_list: common passwords list,
+               try_random_password: variable used when userinput is not in  guess_password_list
+        return: no return value
+        """
+    # check if password is in password list
+    for guess_password in password_list:
+        if get_hash(guess_password) == hashed_password:
             correct_password = guess_password
             answer = tk.Label(root,
                               text="Your password is",
                               font=("Verdana", 20),
                               bg="#000000",
                               fg="#1e00ff")
-            answer.place(x=0, y=310)
+            answer.place(x=160, y=310)
             answer.config(text="Your password:" + correct_password
-                               + "\n is in the common passwords list")
-            # print('your password is:', correct_password)
-            try_random = 1
-
-    if try_random == 0:
+                               + "\n is a common password")
+            try_random_password = 1
+    # else try cracking the password using brute force(guessing)
+    if try_random_password == 0:
         charset = string.ascii_letters + string.digits + string.punctuation
+        # cartesian product, equivalent to a nested for-loop
+        # a set of all ordered pairs between charset and repeat
+        # which is the length of the password input
         for pwd in itertools.product(charset,
                                      repeat=len(password)):
             # add the values without space to the test variable
@@ -53,58 +68,54 @@ def bruteforce(guess_password_list, password_hash, password, try_random):
                                   font=("Verdana", 20),
                                   bg="#000000",
                                   fg="#1e00ff")
-                answer.place(x=0, y=310)
-                answer.config(text="Your password: \n" + test
-                                   + "\n is somewhat safe")
+                answer.place(x=180, y=310)
+                answer.config(text="Your password:" + test
+                                   + "\n was found!")
                 # print('your password is:', test)
                 break
 
 
 def check_password():
     """
-    A function used to show the user's input password
+    A function used to call the functions get_hash()and brute_force
+    so that we can perform the cracking
+    params: none
+    return value: none
     """
     password = enter_text.get()
-    """password_check = tk.Label(root,
-                              text="Hacking...",
-                              font=("Verdana", 20),
-                              bg="#000000",
-                              fg="#1e00ff")
-    password_check.place(x=120, y=310)
-    password_check.config(text="Hacking... \n" + password)
-    """
     with open('password.txt', 'r', encoding='utf-8') as text_file:
         contents = text_file.read()
 
     st = time.time()
-    password_hash = get_hash(password)
-    guess_password_list = contents.split('\n')
-    try_random = 0
+    hashed_password = get_hash(password)
+    password_list = contents.split('\n')
+    try_random_password = 0
     # Running the Brute Force attack
-    bruteforce(guess_password_list, password_hash, password, try_random)
+    brute_force(password_list, hashed_password, password, try_random_password)
 
     # get the end time
     et = time.time()
 
     # get the execution time
     res = et - st
-    # print('Execution time:', final_res, 'milliseconds')
+    
     time_used = tk.Label(root,
                          text="Execution time",
                          font=("Verdana", 20),
                          bg="#000000",
                          fg="#1e00ff")
-    time_used.place(x=350, y=310)
+    time_used.place(x=200, y=450)
     time_used.config(text="Execution time: \n" + str(round(res, 2)) + 'seconds')
 
 
+# style the heading
 heading = tk.Label(root,
                    text="Password Cracker",
                    font=("Verdana", 23, "bold"),
                    bg="#000000",
                    fg="#1e00ff")
 heading.pack(pady=(90, 0))
-
+# style the entry box
 enter_text = tk.Entry(root,
                       justify="center",
                       width=15,
@@ -114,7 +125,7 @@ enter_text = tk.Entry(root,
 enter_text.insert(0, "Enter Password...")
 enter_text.pack(pady=10)
 enter_text.bind("<FocusIn>", temp_text)
-
+# style the button
 button = tk.Button(root,
                    text="Check Password",
                    width=17,
@@ -122,7 +133,11 @@ button = tk.Button(root,
                    fg="#1e00ff",
                    bg="#000000",
                    command=check_password)
-
 button.pack()
 
+# end the loop
 root.mainloop()
+
+# run the code
+if __name__ == '__main__':
+    check_password()
